97d30978346cbbc56807f2090caf5a5ee2f87aaf
Benson Ndiwa <bnkimtai@gmail.com>
2022-03-07 15:37:20 +0300

    Reading and writing files

--- app.js
+++ app.js
@@ -38,13 +38,60 @@
     // console.log(counter(['ben','abu','eliud']));
 
 //module patterns
-    //calling several functions from a file export
-    let stuff = require('./stuff');
+    // calling several functions from a file export
+        // let stuff = require('./stuff');
 
-    console.log(stuff.counter(['ben','abu','eliud']));
+        // console.log(stuff.counter(['ben','abu','eliud']));
 
-    console.log(stuff.adder(2,3));
+        // console.log(stuff.adder(2,3));
 
-    console.log(stuff.pi);
+        // console.log(stuff.pi);
 
-    console.log(stuff.adder(stuff.pi,3));
+        // console.log(stuff.adder(stuff.pi,3));
+
+        // console.log(stuff.multiply(5,4));
+
+
+//require core modules
+    //requiring event module
+        //var events = require('events');
+
+    //Event emitter
+        // let myEmitter = new events.EventEmitter();
+
+        // myEmitter.on('someEvent', function(msg){
+        //     console.log(msg);
+        // });
+
+        // myEmitter.emit('someEvent','the event was emitted');
+
+   // let util = require('util'); //inherits certains things from objects used in nodeJS or other modules
+
+    // let Person = function(name){
+    //     this.name = name;
+    // };
+//     var events = require('events');
+//     class Person extends events.EventEmitter{
+//         constructor(name){
+//             super();
+//             this.name = name;
+//         } 
+//     }
+//    // util.inherits(Person, events.EventEmitter);
+
+//     let james = new Person('james');
+//     let mary = new Person('mary');
+//     let ryan = new Person('ryan');
+
+//     let people = [james, mary, ryan];
+
+//     people.forEach(function(person){
+//         person.on('speak', function(msg){
+//             console.log(person.name + ' said: ' +msg)
+//         })
+//     })
+
+//     james.emit('speak', 'hey dudes');
+//     mary.emit('speak', 'Fuck you');
+
+//Reading and writing files
--- stuff.js
+++ stuff.js
@@ -1,32 +1,36 @@
-let counter = function(arr){
-    return 'There are '+arr.length+ ' elements in this array';
-};
+// let counter = function(arr){
+//     return 'There are '+arr.length+ ' elements in this array';
+// };
 
-let adder = function(x,y){
-    return `The sum is ${x+y}`;
-}
+// let adder = function(x,y){
+//     return `The sum is ${x+y}`;
+// }
 
-let pi  = 3.142;
+// let pi  = 3.142;
 
 
 // module.exports.counter = counter;
 // module.exports.adder = adder;
 // module.exports.pi = pi;
 
-    // We can also do this
-        // module.exports.counter = function(arr){
-        //     return 'There are '+arr.length+ ' elements in this array';
-        // };
-
-        // module.exports.adder = function(x,y){
-        //     return `The sum is ${x+y}`;
-        // }
-
-        // module.exports.pi  = 3.1;
-
     //we can also do:
-    module.exports = {
-        counter: counter,
-        adder: adder,
-        pi: pi,
-    }
+        // module.exports = {
+        //     counter: counter,
+        //     adder: adder,
+        //     pi: pi,
+        // }
+        
+    // We can also do this
+        module.exports.counter = function(arr){
+            return 'There are '+arr.length+ ' elements in this array';
+        };
+
+        module.exports.adder = function(x,y){
+            return `The sum is ${x+y}`;
+        }
+
+        module.exports.pi  = 3.1;
+
+        module.exports.multiply = function(ab, cd){
+            return ab*cd;
+        }
